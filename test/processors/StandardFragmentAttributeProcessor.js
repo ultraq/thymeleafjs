cc8ebcfb51d1eeb5ec1981770aa9b81e382c929f
Emanuel Rabina <emanuelrabina@gmail.com>
2017-10-29 11:26:59 +1300

    Tests for the fragment processor, #8

---
+++ test/processors/StandardFragmentAttributeProcessor.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017, Emanuel Rabina (http://www.ultraq.net.nz/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import StandardFragmentAttributeProcessor from '../../src/standard/processors/StandardFragmentAttributeProcessor';
+import {createThymeleafAttributeValue}    from '../../src/utilities/Dom';
+
+import {assert} from 'chai';
+import h        from 'hyperscript';
+import hh       from 'hyperscript-helpers';
+
+const {div} = hh(h);
+
+/**
+ * Tests for the `th:fragment` attribute processor.
+ */
+describe('processors/StandardFragmentAttributeProcessor', function() {
+
+	let processor;
+	let attribute;
+	before(function() {
+		processor = new StandardFragmentAttributeProcessor('test');
+		attribute = `${processor.name}:${processor.prefix}`;
+	});
+
+	it('Adds encountered fragments to the context (no fragments list)', function() {
+		let attributeValue = 'my-fragment';
+		let element = createThymeleafAttributeValue(div(), attribute, attributeValue);
+
+		let context = {};
+		processor.process(element, attribute, attributeValue, context);
+
+		let {fragments} = context;
+		assert.isArray(fragments);
+		assert.include(fragments[0], { name: attributeValue });
+	});
+
+	it('Adds encountered fragments to the context (existing fragments list)', function() {
+		let attributeValue = 'my-fragment';
+		let element = createThymeleafAttributeValue(div(), attribute, attributeValue);
+
+		let context = {
+			fragments: []
+		};
+		processor.process(element, attribute, attributeValue, context);
+
+		let {fragments} = context;
+		assert.isArray(fragments);
+		assert.include(fragments[0], { name: attributeValue });
+	});
+});
