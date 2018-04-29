8e7569f1de924e82340c0382e7ec93095c0c3f37
Emanuel Rabina <emanuelrabina@gmail.com>
2018-04-29 17:43:15 +1200

    Merge these tests into the expression language file

--- test/standard/expressions/ThymeleafExpressionLanguage.js
+++ test/standard/expressions/ThymeleafExpressionLanguage.js
@@ -14,27 +14,120 @@
  * limitations under the License.
  */
 
-import InputBuffer                 from '../../../source/parser/InputBuffer';
-import Parser                      from '../../../source/parser/Parser';
-import ThymeleafExpressionLanguage from '../../../source/standard/expressions/ThymeleafExpressionLanguage';
+import ExpressionProcessor from '../../../source/standard/expressions/ExpressionProcessor';
 
 /**
  * Tests for the Thymeleaf expression language.
  */
 describe('standard/expressions/ThymeleafExpressionLanguage', function() {
 
-	const parser = new Parser(ThymeleafExpressionLanguage);
+	describe('#VariableExpression', function() {
+		const context = {
+			greeting: 'Good morning!',
+			greetings: {
+				hello: 'Hello :)',
+				goodbye: 'Goodbye :(',
+				goodnight: null
+			}
+		};
+
+		test('Expressions get passed to underlying function', function() {
+			let expressionProcessor = new ExpressionProcessor(context);
+
+			let result = expressionProcessor.process('${greeting}');
+			expect(result).toBe(context.greeting);
+
+			result = expressionProcessor.process('${greetings.hello}');
+			expect(result).toBe(context.greetings.hello);
+		});
+
+		test('null/undefined value handling', function() {
+			let expressionProcessor = new ExpressionProcessor(context);
+			let result = expressionProcessor.process('${greetings.goodnight}');
+			expect(result).toBe('');
+		});
+
+		test('No context handling', function() {
+			let expressionProcessor = new ExpressionProcessor();
+			let result = expressionProcessor.process('${greeting}');
+			expect(result).toBe('');
+		});
+	});
+
+
+	describe('#FragmentExpression', function() {
+		let expressionProcessor;
+		beforeEach(function() {
+			expressionProcessor = new ExpressionProcessor();
+		});
+
+		test('Extracts the template, fragment, and parameter parts', function() {
+			let result = expressionProcessor.process('~{template :: fragment(parameters)}');
+			expect(result).toEqual({
+				templateName: 'template',
+				fragmentName: 'fragment',
+				parameters: '(parameters)'
+			});
+		});
+	});
+
+
+	describe('#IterationExpression', function() {
+		const context = {
+			items: ['a', 'b', 'c']
+		};
+		let expressionProcessor;
+		beforeEach(function() {
+			expressionProcessor = new ExpressionProcessor(context);
+		});
+
+		test('Value and local name mapping', function() {
+			let expression = 'item: ${items}';
+			let result = expressionProcessor.process(expression);
+			expect(result.localValueName).toBe('item');
+			expect(result.iterable).toBe(context.items);
+		});
+	});
+
+
+	describe('#LinkExpression', function() {
+		const context = {
+			greeting: 'hello'
+		};
+		let expressionProcessor;
+		beforeEach(function() {
+			expressionProcessor = new ExpressionProcessor(context);
+		});
+
+		test('Leaves URLs without special parameters alone', function() {
+			let result = expressionProcessor.process('@{/test}');
+			expect(result).toBe('/test');
+		});
+
+		test('Append special parameters', function() {
+			let result = expressionProcessor.process('@{/test(param1=hard-coded-value,param2=${greeting})}');
+			expect(result).toBe('/test?param1=hard-coded-value&param2=hello');
+		});
+
+		test('Replace parameters in url', function() {
+			let result = expressionProcessor.process('@{/{part1}/{part2}/(part1=test,part2=${greeting})}');
+			expect(result).toBe('/test/hello/');
+		});
+
+		test('Mixed template and query parameters', function() {
+			let result = expressionProcessor.process('@{/test/{template}(template=${greeting},query=next)}');
+			expect(result).toBe('/test/hello?query=next');
+		});
+	});
+
 
 	describe('#LogicalExpression', function() {
 
 		test('${var} === literal', function() {
-			let logicalExpressionProcessor = ThymeleafExpressionLanguage.accept(
-				new InputBuffer('${number} === 3'),
-				parser
-			);
-			let result = logicalExpressionProcessor({
+			let processor = new ExpressionProcessor({
 				number: 3
 			});
+			let result = processor.process('${number} === 3');
 			expect(result).toBeTrue();
 		});
 	});
@@ -42,39 +135,45 @@ describe('standard/expressions/ThymeleafExpressionLanguage', function() {
 	describe('#IfThenCondition', function() {
 
 		test('Executes the true branch', function() {
-			let ifThenProcessor = ThymeleafExpressionLanguage.accept(
-				new InputBuffer("${condition} ? 'Hello!'"),
-				parser
-			);
-			let result = ifThenProcessor({
+			let processor = new ExpressionProcessor({
 				condition: true
 			});
+			let result = processor.process("${condition} ? 'Hello!'");
 			expect(result).toBe('Hello!');
 		});
 	});
 
+
 	describe('#IfThenElseCondition', function() {
 
 		test('Executes the true branch', function() {
-			let ifThenElseProcessor = ThymeleafExpressionLanguage.accept(
-				new InputBuffer("${condition} ? 'Hello!' : 'Goodbye :('"),
-				parser
-			);
-			let result = ifThenElseProcessor({
+			let processor = new ExpressionProcessor({
 				condition: true
 			});
+			let result = processor.process("${condition} ? 'Hello!' : 'Goodbye :('");
 			expect(result).toBe('Hello!');
 		});
 
 		test('Executes the false branch', function() {
-			let ifThenElseProcessor = ThymeleafExpressionLanguage.accept(
-				new InputBuffer("${condition} ? 'Hello!' : 'Goodbye :('"),
-				parser
-			);
-			let result = ifThenElseProcessor({
+			let processor = new ExpressionProcessor({
 				condition: false
 			});
+			let result = processor.process("${condition} ? 'Hello!' : 'Goodbye :('");
 			expect(result).toBe('Goodbye :(');
+		});
+	});
+
+
+	describe('#Literals', function() {
+		let expressionProcessor;
+		beforeEach(function() {
+			expressionProcessor = new ExpressionProcessor();
+		});
+
+		test('Verbatim expressions (fallback)', function() {
+			let greeting = 'Hello!';
+			let result = expressionProcessor.process(greeting);
+			expect(result).toBe(greeting);
 		});
 	});
 
